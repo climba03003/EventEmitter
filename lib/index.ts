9f62311368d54b709787e2e6381bf4b070995cc6
climba03003 <climba03003@gmail.com>
2019-11-27 16:59:19 +0800

    Fix: Decorator

--- lib/index.ts
+++ lib/index.ts
@@ -16,7 +16,7 @@ class Listener {
     this.mode = mode;
   }
 
-  async execute(...args: any) {
+  async execute(...args: Array<any>): Promise<any> {
     if (this.mode === MODE_ONCE && this.executed) return true;
     this.executed = true;
     return await this.listener.apply(this.listener, args);
@@ -27,13 +27,15 @@ export class EventEmitter {
   private events: EventStack = {};
   private defaultMaxListeners = 10;
 
-  addListener(eventName: string | symbol, listener: Function) {
-    if (this.ensureStack.length > this.defaultMaxListeners) throw new Error('Maximum Number of Event Listener exceed.');
+  addListener(eventName: string | symbol, listener: Function): this {
+    if (this.ensureStack(eventName).length + 1 > this.defaultMaxListeners) {
+      throw new Error('Maximum Number of Event Listener exceed.');
+    }
     this.ensureStack(eventName).push(new Listener(listener, MODE_ALWAYS));
     return this;
   }
 
-  async emit(eventName: string | symbol, ...args: any) {
+  async emit(eventName: string | symbol, ...args: Array<any>): Promise<boolean> {
     const stack = this.ensureStack(eventName);
     for (let i = 0; i < stack.length; i++) {
       const listener = stack[i];
@@ -42,7 +44,7 @@ export class EventEmitter {
     return true;
   }
 
-  eventNames() {
+  eventNames(): Array<string> {
     return Object.keys(this.events);
   }
 
@@ -50,43 +52,43 @@ export class EventEmitter {
     return this.defaultMaxListeners;
   }
 
-  listenerCount(eventName: string | symbol) {
+  listenerCount(eventName: string | symbol): number {
     return this.ensureStack(eventName).length;
   }
 
-  listeners(eventName: string | symbol) {
+  listeners(eventName: string | symbol): Array<Listener> {
     return this.ensureStack(eventName);
   }
 
-  off(eventName: string | symbol, listener: Function) {
+  off(eventName: string | symbol, listener: Function): this {
     return this.removeListener(eventName, listener);
   }
 
-  on(eventName: string | symbol, listener: Function) {
+  on(eventName: string | symbol, listener: Function): this {
     return this.addListener(eventName, listener);
   }
 
-  once(eventName: string | symbol, listener: Function) {
+  once(eventName: string | symbol, listener: Function): this {
     this.ensureStack(eventName).push(new Listener(listener, MODE_ONCE));
     return this;
   }
 
-  prependListener(eventName: string | symbol, listener: Function) {
+  prependListener(eventName: string | symbol, listener: Function): this {
     this.ensureStack(eventName).unshift(new Listener(listener, MODE_ALWAYS));
     return this;
   }
 
-  prependOnceListener(eventName: string | symbol, listener: Function) {
+  prependOnceListener(eventName: string | symbol, listener: Function): this {
     this.ensureStack(eventName).unshift(new Listener(listener, MODE_ONCE));
     return this;
   }
 
-  removeAllListeners(eventName: string | symbol) {
+  removeAllListeners(eventName: string | symbol): this {
     delete this.events[eventName as string];
     return this;
   }
 
-  removeListener(eventName: string | symbol, listener: Function) {
+  removeListener(eventName: string | symbol, listener: Function): this {
     const stack = this.ensureStack(eventName);
     let index = this.indexOfListener(stack, listener);
     while (index >= 0) {
@@ -96,24 +98,24 @@ export class EventEmitter {
     return this;
   }
 
-  setMaxListeners(n: number) {
+  setMaxListeners(n: number): void {
     if (isNaN(n)) throw new Error('MaxListerners must be a number.');
     this.defaultMaxListeners = parseInt(String(n), 10);
   }
 
-  rawListeners(eventName: string | symbol) {
+  rawListeners(eventName: string | symbol): Array<Function> {
     return this.ensureStack(eventName).map(function(listener) {
       return listener.listener;
     });
   }
 
-  private ensureStack(eventName: string | symbol) {
+  private ensureStack(eventName: string | symbol): Array<Listener> {
     if (!Array.isArray(this.events[eventName as string])) this.events[eventName as string] = [];
     return this.events[eventName as string];
   }
 
   private indexOfListener(stack: Array<Listener>, listener: Function): number {
-    return stack.findIndex(function(stackListener: Listener, index: number, self: Array<Listener>) {
+    return stack.findIndex(function(stackListener: Listener) {
       return stackListener.listener === listener;
     });
   }
